38c3f7a6257ac574d06cf058c4c954f284bb4ad9
edgar <edgar.andre@ynov.com>
2025-01-28 11:16:48 +0100

    fix: backend launch

--- backend/router/zone-router.js
+++ backend/router/zone-router.js
@@ -1,14 +1,13 @@
 import express from 'express'
-
 import { getZone, getZoneById, createZone, updateZone, deleteZone } from '../controllers/zone-controller.js'
 
 const router = express.Router()
 
-// TODO - add authentication middleware
-router.get('/', null, getZone);
-router.get('/:id', null, getZoneById);
-router.post('/', null, createZone);
-router.put('/:id', null, updateZone);
-router.delete('/:id', null, deleteZone);
+// Define routes with their respective callback functions
+router.get('/', getZone)
+router.get('/:id', getZoneById)
+router.post('/', createZone)
+router.put('/:id', updateZone)
+router.delete('/:id', deleteZone)
 
 export default router
--- backend/services/zone-service.js
+++ backend/services/zone-service.js
@@ -1,4 +1,4 @@
-import prisma from "../db";
+import prisma from "../db.js";
 
 export const getAll = async () => {
     return await prisma.zones.findMany()
