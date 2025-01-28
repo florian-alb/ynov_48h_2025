0f6bd51df10c72a1730380d64f7ac47d046aeafb
Theo Stoffelbach <theo.stoffelbach@ynov.com>
2025-01-28 10:33:12 +0100

    Add chat Page

--- frontend/src/pages/public/Chat.tsx
+++ frontend/src/pages/public/Chat.tsx
@@ -2,14 +2,11 @@ import {Base} from "@/layouts/base.tsx";
 import ContainerChat from "@/components/chat/ContainerChat.tsx";
 
 const Chat = () => {
-    // const [selectedChat, setSelectedChat] = useState(1);
-
     return (
         <Base>
             <div
                 className="flex min-h-svh w-full justify-center p-6 md:p-10">
                 <div className="w-full flex justify-center">
-                    {/*<SelectChat chat={selectedChat} setChat={setSelectedChat}/>*/}
                     <ContainerChat/>
                 </div>
             </div>
